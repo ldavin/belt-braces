a69e642804b312c6fff85454cd32f1b01ef0e96b
Louis Davin <ldavin@octo.com>
2016-11-03 15:58:58 +0100

    Add empty integration test

--- src/test/kotlin/io/ldavin/beltsbraces/BeltsTest.kt
+++ src/test/kotlin/io/ldavin/beltsbraces/BeltsTest.kt
@@ -1,13 +1,15 @@
 package io.ldavin.beltsbraces
 
 import io.ldavin.beltsbraces.exception.FastenYourSeatBeltException
+import io.ldavin.beltsbraces.exception.NoAssertionFoundException
+import io.ldavin.beltsbraces.fixture.JEmpty
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class BeltsTest {
 
     @Test
-    fun integrationTest() {
+    fun `Integration test with a simple class`() {
         // GIVEN
         val testObject = TestClass("Hi!")
 
@@ -22,6 +24,15 @@ class BeltsTest {
         // THEN
         assertThat(catched!!.message).hasLineCount(3)
         assertThat(catched.message).contains("assertThat(result.getAttribute()).isEqualTo(\"Hi!\")")
+    }
+
+    @Test(expected = NoAssertionFoundException::class)
+    fun `Integration test with an empty class`() {
+        // GIVEN
+        val testObject = JEmpty()
+
+        // WHEN
+        BeltsAndBraces.fasten(testObject)
     }
 
     data class TestClass(val attribute: String)
